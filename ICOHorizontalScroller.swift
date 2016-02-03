c69f69d129b20680e55ff3164269ee43fe231d5b
xtabrisx <cice@aula1povm15.local>
2016-02-03 09:14:01 +0100

    tedt

---
+++ ICOHorizontalScroller.swift
@@ -0,0 +1,15 @@
+//
+//  ICOHorizontalScroller.swift
+//  App_MVC_Complete
+//
+//  Created by cice on 3/2/16.
+//  Copyright © 2016 djb. All rights reserved.
+//
+
+import UIKit
+
+class ICOHorizontalScroller: UIView {
+
+    
+
+}
